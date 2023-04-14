2902f5e767069112157b0e73b366e2f3c1c3889f
hung1111234 <95286618+hung1111234@users.noreply.github.com>
2023-04-14 22:29:32 +0700

    Update trolyao.py

--- trolyao.py
+++ trolyao.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import playsound
 import speech_recognition as sr
 import time
@@ -26,87 +27,39 @@ from youtubesearchpython import VideosSearch
 import pyttsx3
 from googlesearch import search
 from gtts import gTTS
-
+from tkinter import *
+# import a
 #%% ngon ngu
 wikipedia.set_lang('vi')
 language = 'vi'
-path = ChromeDriverManager().install()
-flag = 1
-
-print("Trợ lý ảo: Tôi có thể giao tiếp với bạn qua 2 cách sau:")
-print("Chọn 1: Tôi sẽ giao tiếp với bạn qua giọng nói")
-print("Chọn 2: Tôi sẽ giao tiếp với bạn qua tin nhắn")
-print("Vui lòng nhập: ", end='')
-while True:
-    flag = int(input())
-    if flag != 1 and flag != 2:
-        print("Bạn đã nhập sai, vui lòng nhập lại: ", end='')
-    else: break
-
-#%%
-def speak(text):
-    print("Trợ lý ảo: {}".format(text))
-    if flag == 1:
-        tts = gTTS(text = text,lang='vi')
-        tts.save("sound.mp3")
-        playsound.playsound("sound.mp3")
-        os.remove("sound.mp3")
-        # engine = pyttsx3.init()
-        # voices = engine.getProperty('voices')
-        # engine.setProperty('voice', voices[1].id)
-        # engine.say(text)
-        # engine.runAndWait()
+flag = True
+# path = ChromeDriverManager().install()
 
 
 #%%
-def get_audio():
-    if flag == 1:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Tôi: ", end='')
-            audio = r.record(source, duration=5)
-            try:
-                text = r.recognize_google(audio, language="vi-VN")
-                print(text)
-                return text
-            except:
-                print()
-                speak("Nếu bạn không tiện nói bạn có thể nhập")
-                text = input("Tôi: ")
-                return text
-    else:
-        print("Tôi: ", end='')
-        text = input()
-        return text
-
-#%%
-def stop():
-    speak("Hẹn gặp lại bạn sau!")
-    
-#%%
-def get_text():
-    for i in range(10000):
-        text = get_audio()
-        if text:
-            return text.lower()
-        # elif flag == 1:
-        #     speak("Tôi không nghe rõ. Bạn nói lại được không!")
-        elif flag == 2:
-            speak("Bạn đã nhập sai, vui lòng nhập lại:")
-    time.sleep(2)
-    stop()
-    return None   
-
-#%%
-def hello(name):
+def hello():
+    speak("Xin chào bạn, bạn tên là gì ạ!")
+    while True:
+        text = record()
+        if text == '...': 
+            speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+        else: break
+    name = text.split()[-1]
     day_time = int(strftime('%H'))
     if day_time < 12:
         speak("Chào buổi sáng bạn {}. Chúc bạn một ngày tốt lành.".format(name))
     elif 12 <= day_time < 18:
         speak("Chào buổi chiều bạn {}. Bạn đã dự định gì cho chiều nay chưa.".format(name))
     else:
-        speak("Chào buổi tối bạn {}. Bạn đã ăn tối chưa nhỉ.".format(name))
-        
+        speak("Chào buổi tối bạn {}. Chúc bạn một buổi tối vui vẻ.".format(name))
+    speak("Bạn có khỏe không?")
+    while True:
+        text = record()
+        if text == '...': 
+            speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+        else: break
+    
+
 #%%
 def get_time(text):
     now = datetime.datetime.now()
@@ -116,12 +69,16 @@ def get_time(text):
         speak("Hôm nay là ngày %d tháng %d năm %d" %
               (now.day, now.month, now.year))
     else:
-        speak("Tôi chưa hiểu ý của bạn. Bạn nói lại được không?")
+        speak("Tôi không hiểu ý của bạn. Bạn nói lại được không?")
 
 def search_youtube():
-    if flag == 1: speak("Vui lòng nói từ khóa tìm kiếm:")
-    if flag == 2: speak("Vui lòng nhập từ khóa tìm kiếm:")
-    text = get_text()
+    speak("Vui lòng nói từ khóa tìm kiếm mà bạn muốn xem")
+    while True:
+        text = record()
+        if text == '...': 
+            speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+        else: break
+    text_widget.insert(END, "\n\n")
     search = VideosSearch(text)
 
     # Lấy kết quả tìm kiếm
@@ -134,48 +91,58 @@ def search_youtube():
         webbrowser.open(random_link)
         speak("Video đã mở thành công")
     else:
-        print("Không tìm thấy kết quả phù hợp trên YouTube")
+        speak("Không tìm thấy kết quả phù hợp trên YouTube")
 
 #%%
 def open_application(text):
-    if "google" in text:
-        speak("Mở Google Chrome")
+    if "google" in text: 
         os.startfile(r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
-    elif "zalo" in text:
-        speak("Mở Zalo")
+        speak("Đã mở Google Chrome")
+    
+    elif "zalo" in text:    
         os.startfile(r'C:\Users\ADMIN\AppData\Local\Programs\Zalo\Zalo.exe')
+        speak("Đã mở Zalo")
+    
     elif "word" in text:
-        speak("Mở Microsoft Word")
         os.startfile(r"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE")
+        speak("Đã mở Microsoft Word")
+    
     elif "excel" in text:
-        speak("Mở Microsoft Excel")
         os.startfile(r"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE")
+        speak("Đã mở Microsoft Excel")
+    
     elif "powerpoint" in text or "thuyết trình" in text:
-        speak("Mở PowerPoint")
         os.startfile(r"C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE")     
-    elif "zoom" in text:
-        speak("Mở Zoom")
+        speak("Đã mở PowerPoint")
+    
+    elif "zoom" in text:    
         os.startfile(r"C:\Users\ADMIN\AppData\Roaming\Zoom\bin\Zoom.exe")     
+        speak("Đã mở Zoom")
     elif "c++" in text:
-        speak("Mở Dev C++")
         os.startfile(r"C:\Program Files (x86)\Dev-Cpp\devcpp.exe")     
-    elif "java" in text:
-        speak("Mở Apache NetBeans")
+        speak("Đã mở Dev C++")
+    elif "java" in text:      
         os.startfile(r"C:\Program Files\NetBeans-15\netbeans\bin\netbeans64.exe")
+        speak("Đã mở Apache NetBeans")
     else:
-        speak("Ứng dụng chưa được cài đặt. Bạn hãy thử lại!")
+        speak("Ứng dụng chưa được cài đặt hoặc tôi chưa hiểu ý bạn. Bạn hãy thử lại!")
+
     
 #%%
 def open_google_and_search():
-    if flag == 1: speak("Vui lòng nói từ khóa tìm kiếm:")
-    if flag == 2: speak("Vui lòng nhập từ khóa tìm kiếm:")
-    query = get_text()
+    speak("Vui lòng nói từ khóa tìm kiếm trên google:")
+    while True:
+        text = record()
+        if text == '...': 
+            speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+        else: break
+    text_widget.insert(END, "\n\n")
     # Tìm kiếm trên Google và lưu trữ các kết quả trong danh sách 'search_results'
     try:
-        search_results = list(search(query, num_results=10))
+        search_results = list(search(text, num_results=10))
         # Lấy ngẫu nhiên một đường link trong danh sách kết quả tìm kiếm
         if len(search_results) > 0:
-            random_link = random.choice(search_results)
+            random_link = search_results[0]
             webbrowser.open(random_link)
             speak("Trang web đã mở thành công")
     except: speak("Tôi không tìm thấy kết quả này trên google")
@@ -183,13 +150,17 @@ def open_google_and_search():
 #%%
 def current_weather():
     speak("Bạn muốn xem thời tiết ở đâu ạ.")
+    while True:
+        text = record()
+        if text == '...': 
+            speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+        else: break
+    text_widget.insert(END, "\n\n")
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    time.sleep(2)
-    city = get_text()
     if not city:
         pass
     api_key = "5f9242d740d1e6278e390d5fd21a3881"
-    call_url = base_url + "appid=" + api_key + "&q=" + city 
+    call_url = base_url + "appid=" + api_key + "&q=" + city
     response = requests.get(call_url)
     data = response.json()
     if data["cod"] != "404":
@@ -209,7 +180,7 @@ def current_weather():
         Trời hôm nay quang mây. Dự báo mưa rải rác ở một số nơi.""".format(day = now.day,month = now.month, year= now.year, 
                                                                            temp = current_temperature, pressure = current_pressure, humidity = current_humidity)
         speak(content)
-        time.sleep(20)
+
     else:
         speak("Tôi không tìm thấy địa chỉ nơi bạn muốn biết thời tiết")
 
@@ -217,15 +188,20 @@ def current_weather():
 def read_news():
     
     speak("Bạn muốn đọc báo về gì")
-    
-    queue = get_text()
+
+    while True:
+        queue = record()
+        if queue == '...': 
+            speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+        else: break
+    text_widget.insert(END, "\n\n")
     params = {
         'apiKey': '9007bba8aca54b379ccba2964abac887',
         'q': queue,
     }
     api_result = requests.get('http://newsapi.org/v2/top-headlines?', params)
     api_response = api_result.json()
-    print("Tin tức")
+    speak("Tin tức")
 
     for number, result in enumerate(api_response['articles'], start=1):
         print(f"""Tin {number}:\nTiêu đề: {result['title']}\nTrích dẫn: {result['description']}\nLink: {result['url']}\nNội dung: {result['content']}
@@ -237,75 +213,174 @@ def read_news():
 def tell_me_about():
     try:
         speak("Bạn muốn nghe về gì ạ")
-        time.sleep(2)
-        text = get_text()
+        while True:
+            text = record()
+            if text == '...': 
+                speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+            else: break
+        # text_widget.insert(END, "\n\n")
         contents = wikipedia.summary(text).split('\n')
         speak(contents[0])
-        time.sleep(10)
         for content in contents[1:]:
             speak("Bạn muốn nghe thêm không")
-            ans = get_text()
-            if "có" not in ans:
+            while True:
+                text = record()
+                if text == '...': 
+                    speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+                else: break
+            if "có" not in text:
                 break    
             speak(content)
-            time.sleep(10)
-        time.sleep(3)
+
         speak('Cảm ơn bạn đã lắng nghe!!!')
     except:
-        speak("Tôi không định nghĩa được thuật ngữ của bạn. Xin mời bạn thực hiện lại thao tác")
+        speak("Tôi không tìm thấy thông tin của thuật ngữ mà bạn cần.")
         
 #%%
 def help_me():
-    speak("Tôi có thể giúp bạn thực hiện các câu lệnh sau đây:")
-    print("""
-    1. Chào hỏi
-    2. Hiển thị giờ
-    3. Mở Application
-    4. Tìm kiếm trên Google
-    5. Dự báo thời tiết
-    6. Mở video trên youtube
-    7. Đọc báo hôm nay
-    8. Tìm thông tin của một thuật ngữ, một người""")
-    time.sleep(2)
+
+    speak("""Tôi có thể giúp bạn thực hiện các chức năng sau đây:
+        1. Chào hỏi
+        2. Hiển thị ngày hoặc giờ
+        3. Mở ứng dụng
+        4. Mở google và tìm kiếm
+        5. Dự báo thời tiết
+        6. Xem video, nghe nhạc trên youtube
+        7. Đọc báo hôm nay
+        8. Tìm thông tin của một thuật ngữ hoặc một người""")
 
 #%%
-def assistant():
-    speak("Xin chào, bạn tên là gì nhỉ?")
-    time.sleep(1)
-    s = get_text().split()
-    name = s[-1]
-    if name:
-        speak("Chào bạn {}".format(name[0:1].upper() + name[1:]))
-        time.sleep(2)
-        speak("Bạn cần tôi giúp gì ạ?")
-        while True:
-            time.sleep(3)
-            text = get_text()
-            if not text:
-                break
-            elif "dừng" in text or "stop" in text or "tạm biệt" in text or "ngủ thôi" in text:
-                stop()
-                break
-            elif "làm gì" in text or 'làm được gì' in text:
-                help_me()
-            elif "chào trợ lý ảo" in text or "Xin chào" in text:
-                hello(name)
-            elif "hiện tại" in text or "mấy giờ" in text:
-                get_time(text)
-            elif "tìm kiếm" in text or "trên google" in text:
-                open_google_and_search()
-            elif "youtube" in text or "video" in text or "bài hát" in text or "nhạc" in text or "nghe" in text:
-                search_youtube()
-            elif "mở" in text or "ứng dụng" in text:
-                open_application(text)
-            elif "thời tiết" in text:
-                current_weather()
-            elif "đọc báo" in text:
-                read_news()
-            elif "thuật ngữ" in text or "cho tôi biết" in text or "tôi muốn biết" in text or "thông tin" in text:
-                tell_me_about()
-            else:
-                speak("Bạn cần tôi giúp gì ạ?")
-                help_me()
-#%%
-assistant()
+def assistant(text):
+    global flag
+    flag = False
+    ok = 0
+    if text == "" or text == '...':
+        speak("Tôi không nghe rõ, bạn có thể nói lại được không")
+    elif "làm gì" in text or 'làm được gì' in text:
+        help_me()
+        ok = 1
+    elif "chào" in text or "hello" in text:
+        hello()
+        ok = 1
+    elif "hiện tại" in text or "giờ" in text or "ngày" in text:
+        get_time(text)
+        ok = 1
+    elif "tìm kiếm" in text or "trên google" in text or "web" in text:
+        open_google_and_search()
+        ok = 1
+    elif "mở" in text or "open" in text or "ứng dụng" in text:
+        open_application(text)
+        ok = 1
+    elif "youtube" in text or "nhạc" in text or "video" in text or "bài hát" in text or "xem" in text:
+        search_youtube()
+        ok = 1
+    elif "thời tiết" in text:
+        current_weather()
+        ok = 1
+    elif "đọc báo" in text:
+        read_news()
+        ok = 1
+    elif "định nghĩa" in text or "cho tôi biết" in text or "tôi muốn biết" in text or "thông tin" in text:
+        tell_me_about()
+        ok = 1
+    if ok:
+        speak("Bạn cần tôi giúp gì nữa không ạ?")
+    else: 
+        speak("Tôi chưa hiểu ý của bạn, bạn có thể nói lại giúp tôi được không?")
+            
+    text_widget.configure(state=DISABLED)
+    flag = True
+                
+
+def speak(text):
+    tts = gTTS(text = text,lang='vi')
+    tts.save("sound.mp3")
+    playsound.playsound("sound.mp3")
+    os.remove("sound.mp3")
+    Bot_say(text, text_widget)
+
+def Bot_say(text, text_widget):
+    text_widget.configure(state=NORMAL)
+    text_widget.insert(END, f"Trợ lý ảo: {text}\n\n")
+    text_widget.configure(state=DISABLED)
+    text_widget.see(END)
+
+def You_say(text, text_widget):
+    text_widget.configure(state=NORMAL)
+    text_widget.insert(END, f"Tôi: {text}\n\n")
+    text_widget.see(END)
+    # text_widget.configure(state=DISABLED)
+    if flag == True: assistant(text.lower())
+    
+def _on_enter_pressed(event):
+    msg = msg_entry.get()
+    msg_entry.delete(0, END)
+    You_say(msg, text_widget)
+
+def start_recording():
+    record_button.config(text="Listening...", state=DISABLED)
+    t = threading.Thread(target=record)
+    t.start()
+
+def record():
+    # Ghi âm giọng nói và chuyển đổi thành văn bản
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.record(source, duration=5)
+    try:
+        text = r.recognize_google(audio, language="vi-VN")
+        You_say(text, text_widget)
+        return text
+    except Exception:
+        You_say("...", text_widget)
+    finally:
+        # Sau khi ghi âm hoàn thành, chuyển đổi trạng thái của nút Record trở lại
+        record_button.config(text="Record", state=NORMAL)
+
+BG_GRAY="#ABB2B9"
+BG_COLOR="#17202A"
+TEXT_COLOR = "#EAECEE"
+
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
+window = Tk()
+window.title("A+ Python")
+window.configure(width=600, height=600, bg=BG_COLOR)
+# head label
+head_lable = Label(window, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10)
+head_lable.place(relheight=1)
+
+#tiny divider
+line = Label(window, width=450, bg=BG_GRAY)
+line.place(relwidth=1, rely=0.07, relheight=0.012)
+
+#text widget
+text_widget = Text(window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT_BOLD, padx=5, pady=5)
+text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+
+text_widget.configure(cursor="arrow", state=DISABLED)
+
+# scroll bar
+scrollbar = Scrollbar(text_widget)
+scrollbar.place(relheight=1, relx=0.974)
+scrollbar.configure(command=text_widget.yview)
+
+#bottom label 
+bottom_label = Label(window, bg=BG_GRAY, height=80)
+bottom_label.place(relwidth=1, rely=(0.825))
+
+# message entry box
+msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+msg_entry.focus()
+msg_entry.bind("<Return>", lambda event: _on_enter_pressed(event))
+
+#send button
+send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY, command=lambda: _on_enter_pressed(None))
+send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+
+# record button
+record_button = Button(bottom_label, text="Record", font=FONT_BOLD, width=20, bg=BG_GRAY, command=lambda: start_recording())
+record_button.place(relx=0.54, rely=0.008, relheight=0.06, relwidth=0.22)   
+
+window.mainloop()
