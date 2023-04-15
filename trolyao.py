3b4c17818a292cbdab4bdb1a1a581bf0e57024c7
hung1111234 <95286618+hung1111234@users.noreply.github.com>
2023-04-15 16:57:15 +0700

    Update trolyao.py

--- trolyao.py
+++ trolyao.py
@@ -192,17 +192,18 @@ def read_news():
         if queue == '...': 
             speak("Tôi không nghe rõ, bạn có thể nói lại được không")
         else: break
-    text_widget.insert(END, "\n\n")
     params = {
         'apiKey': '9007bba8aca54b379ccba2964abac887',
         'q': queue,
     }
     api_result = requests.get('http://newsapi.org/v2/top-headlines?', params)
     api_response = api_result.json()
+    if len(api_response['articles']) == 0:
+        speak("Tôi không tìm thấy tin tức về chủ đề này.")
+        return
     speak("Tin tức")
-
     for number, result in enumerate(api_response['articles'], start=1):
-        print(f"""Tin {number}:\nTiêu đề: {result['title']}\nTrích dẫn: {result['description']}\nLink: {result['url']}\nNội dung: {result['content']}
+        speak(f"""Tin {number}:\nTiêu đề: {result['title']}\nTrích dẫn: {result['description']}\nLink: {result['url']}\nNội dung: {result['content']}
     """)
         if number <= 3:
             webbrowser.open(result['url'])
